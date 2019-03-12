8dde445a3e5bd47406bcde95a589fd062f4c600e
Sean Lintern <sean.lintern88@googlemail.com>
2019-03-12 18:37:43 +0000

    Added some test Data

---
+++ LloydsLastFMTestTests/AlbumDetailsViewModelTest.swift
@@ -0,0 +1,15 @@
+//
+//  AlbumDetailsViewModelTest.swift
+//  LloydsLastFMTestTests
+//
+//  Created by Sean Lintern on 12/03/2019.
+//  Copyright © 2019 Sean Lintern LTD. All rights reserved.
+//
+
+import XCTest
+@testable import LloydsLastFMTest
+
+class AlbumDetailsViewModelTest: XCTestCase {
+
+    
+}

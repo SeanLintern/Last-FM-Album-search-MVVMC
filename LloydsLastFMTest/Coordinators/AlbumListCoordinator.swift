bbf7e677ad39dd0ce7b9582977536eb6a5b85e32
Sean Lintern <sean.lintern88@googlemail.com>
2019-03-12 11:31:05 +0000

    Added the album list view controller.

--- LloydsLastFMTest/Coordinators/AlbumListCoordinator.swift
+++ LloydsLastFMTest/Coordinators/AlbumListCoordinator.swift
@@ -11,6 +11,16 @@ import Foundation
 class AlbumListCoordinator: Coordinator {
     
     func start() {
+        let controller = AlbumListViewController(delegate: self)
+        navigationController.pushViewController(controller, animated: true)
+        fetchAlbumList(controller: controller)
+    }
+    
+    private func fetchAlbumList(controller: AlbumListViewController) {
         
     }
+}
+
+extension AlbumListCoordinator: AlbumListViewControllerDelegate {
+    
 }
---
+++ LloydsLastFMTest/ViewControllers/AlbumListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AlbumListViewController.swift
+//  LloydsLastFMTest
+//
+//  Created by Sean Lintern on 12/03/2019.
+//  Copyright © 2019 Sean Lintern LTD. All rights reserved.
+//
+
+import UIKit
+
+protocol AlbumListViewControllerDelegate: class {
+    
+}
+
+class AlbumListViewController: UIViewController {
+
+    private weak var delegate: AlbumListViewControllerDelegate?
+    
+    init(delegate: AlbumListViewControllerDelegate) {
+        self.delegate = delegate
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}

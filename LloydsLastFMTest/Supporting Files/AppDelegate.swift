f418a5ea22a45ef5afb7728fd8b8478ae20b2aee
Sean Lintern <sean.lintern88@googlemail.com>
2019-03-12 11:07:18 +0000

    Initial commit: Project creation and boilerplate clean up.

---
+++ LloydsLastFMTest/Supporting Files/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  LloydsLastFMTest
+//
+//  Created by Sean Lintern on 12/03/2019.
+//  Copyright © 2019 Sean Lintern LTD. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+
+        return true
+    }
+}
+
